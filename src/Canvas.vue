7974b7448e7baf1ba184b2267d333b5e247b48de
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-08-21 14:07:28 -0500

    Replaced with built in canvas html element.

--- src/Canvas.vue
+++ src/Canvas.vue
@@ -1,20 +1,54 @@
 <script setup lang="ts">
-import Pixel from './Pixel.vue';
+import { onMounted, ref } from 'vue';
 
-defineProps<{
+const props = defineProps<{
   size: number
 }>()
+
+const canvasRef = ref<HTMLCanvasElement | null>(null)
+const canvasBoundingRect = ref<any | null>(null)
+
+const draw = (ctx: CanvasRenderingContext2D, e: any) => {
+    ctx.fillStyle = '#000000'
+    console.log(ctx)
+    console.log(e)
+    const x = (e.pageX - canvasRef.value!.offsetLeft)
+    const y = (e.pageY - canvasRef.value!.offsetTop)
+    console.log(
+        'x: ' + x + '\n' + 
+        'e.pageX: ' + e.pageX + '\n' + 
+        'canvasRef.value!.offsetLeft: ' + canvasRef.value!.offsetLeft + '\n' +
+        'canvasRef.value!.width: ' + canvasRef.value!.width + '\n' +
+        'canvasRef.value!.offsetWidth: ' + canvasRef.value!.offsetWidth + '\n'
+    )
+    console.log(
+        'y: ' + y + '\n' + 
+        'e.pageY: ' + e.pageY + '\n' + 
+        'canvasRef.value!.offsetTop: ' + canvasRef.value!.offsetTop + '\n' + 
+        'canvasRef.value!.height: ' + canvasRef.value!.height + '\n' +
+        'canvasRef.value!.offsetHeight: ' + canvasRef.value!.offsetHeight + '\n' 
+    )
+
+
+    
+    ctx.fillRect(x, y, 10, 10)
+}
+
+function fillPixel(e: any) {
+    const context = canvasRef.value!.getContext('2d')
+    draw(context!, e)
+}
+
+onMounted(() => {
+    canvasRef.value!.width = canvasRef.value!.offsetWidth
+    canvasRef.value!.height = canvasRef.value!.offsetWidth
+    const context = canvasRef.value!.getContext('2d')
+    canvasBoundingRect.value = canvasRef!.value?.getBoundingClientRect()
+
+})
 
 </script>
 
 <template>
-    <template v-if="!size">
-        <h1>Loading!</h1>
-    </template>
-    <template v-else-if="size < 1 ">
-        <h1>Canvas is blank.</h1>
-    </template>
-    <template v-else>
-        <Pixel class="pixel" v-for="pixel in size * size" :key="'pixel' + pixel"></Pixel>
-    </template>
+    <canvas @click="fillPixel" ref="canvasRef"></canvas>
 </template>
