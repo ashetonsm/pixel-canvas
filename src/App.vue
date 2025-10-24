548d3d5b1916337ee2fe9dc8cd99dd3cd0cd0699
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-10-24 20:41:29 +0900

    Added some file naming patterns

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@ import { ref } from 'vue';
 import Canvas from './Canvas.vue';
 
 const color = ref<string>('#000000')
+let imgURL = "https://google.com"
+let thingType = "pet"
+let thingName
+let createdBy = ""
 
 function resetCanvas() {
   try {
@@ -26,12 +30,22 @@ function handleColor(e: Event) {
 
 function handleSubmit() {
   try {
-    const canvas = document.querySelector('canvas')
-    if (canvas) {
-      const image = canvas.toDataURL('image/png')
-      console.log(image)
+    thingName = prompt(`Name your ${thingType}:`)
+
+    if (thingName) {
+      const canvas = document.querySelector('canvas')
+      if (canvas) {
+        const date = new Date().toUTCString().replace(',', '').replace(/ /g,"-")
+        const image = canvas.toDataURL('image/png')
+        const fimg = document.querySelector('#finishedImg')
+        fimg!.href = image;
+        fimg!.download = `${thingType}-${thingName}-${date}`; // Or any desired filename
+        console.log(image)
+      } else {
+        console.log("Canvas not found!")
+      }
     } else {
-      console.log("Canvas not found!")
+      alert(`You must name your ${thingType}! Please try again.`)
     }
   } catch (error : any) {
     console.log(error)
@@ -45,6 +59,9 @@ function handleSubmit() {
     <div>
       <button @click="resetCanvas">Reset</button>
       <button @click="handleSubmit">Finish</button>
+    </div>
+    <div>
+      <a href="finishedImg" id="finishedImg" download>My Finished Image</a>
     </div>
     <div>
       <h3>Colors:</h3>
