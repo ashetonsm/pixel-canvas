90675a8d02d78c5e7801e99934e675100d5fab8c
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-16 12:04:55 +0900

    Added reference types and test config area.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
+/// <reference types="vitest/config" />
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -14,5 +14,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+  test: {
+
   },
 })
