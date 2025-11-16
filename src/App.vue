0b69677ed835c4bf86c78ab84e2ebf170328d195
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-16 10:43:08 +0900

    Replaced placeholder and route-specific text. Added new buttons.

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,90 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import Canvas from './Canvas.vue';
+import { ref } from "vue";
+import Canvas from "./Canvas.vue";
+import { saveDrawing } from "./tools/saveDrawing";
 
-const color = ref<string>('#000000')
-let imgURL = "https://google.com"
-let thingType = "pet"
-let thingName
-let createdBy = ""
+const color = ref<string>("rgb(0, 0, 0)");
+let drawingName: string | null;
+var lastColor = ref<string>("rgb(0, 0, 0)");
 
 function resetCanvas() {
   try {
-    const canvas = document.querySelector('canvas')
+    const canvas = document.querySelector("canvas");
     if (canvas) {
-      const context = canvas.getContext('2d')
-      context!.clearRect(0, 0, canvas.width, canvas.height)
+      const context = canvas.getContext("2d");
+      context!.fillStyle = "rgb(255, 255, 255)";
+      context!.fillRect(0, 0, canvas.width, canvas.height);
     } else {
-      console.log("Canvas not found!")
+      console.log("Canvas not found!");
     }
-  } catch (error : any) {
-    console.log(error)
-  }}
+  } catch (error: any) {
+    console.log(error);
+  }
+}
 
 function handleColor(e: Event) {
+  console.log(e.target);
   if (e.target) {
-    color.value = (e.target as HTMLInputElement).value.toString()
-    console.log(color.value)
+    lastColor.value = color.value;
+    color.value = (e.target as HTMLInputElement).value.toString();
   }
 }
 
-function handleSubmit() {
-  try {
-    thingName = prompt(`Name your ${thingType}:`)
-
-    if (thingName) {
-      const canvas = document.querySelector('canvas')
-      if (canvas) {
-        const date = new Date().toUTCString().replace(',', '').replace(/ /g,"-")
-        const image = canvas.toDataURL('image/png')
-        const fimg = document.querySelector('#finishedImg')
-        fimg!.href = image;
-        fimg!.download = `${thingType}-${thingName}-${date}`; // Or any desired filename
-        console.log(image)
-      } else {
-        console.log("Canvas not found!")
-      }
-    } else {
-      alert(`You must name your ${thingType}! Please try again.`)
-    }
-  } catch (error : any) {
-    console.log(error)
+async function handleSubmit() {
+  drawingName = prompt(`Name your drawing:`);
+  if (drawingName) {
+    saveDrawing(drawingName);
+  } else {
+    alert(`You must name your drawing! Please try again.`);
   }
 }
-
 </script>
 
 <template>
   <div class="navbar">
     <div>
       <button @click="resetCanvas">Reset</button>
-      <button @click="handleSubmit">Finish</button>
+      <button @click="handleSubmit()">Finish</button>
     </div>
-    <div>
-      <a href="finishedImg" id="finishedImg" download>My Finished Image</a>
-    </div>
-    <div>
-      <h3>Colors:</h3>
-      <button class="black color" value="#000000" @click="handleColor($event)"></button>
-      <button class="white color" value="#FFFFFF" @click="handleColor($event)"></button>
+    <div class="container-block">
+      <div class="row">
+        <h3>Colors:</h3>
+      </div>
+      <div class="row">
+        <div class="column">
+          <button
+            class="black color"
+            value="rgb(0, 0, 0)"
+            @click="handleColor($event)"
+            style="background-color: rgb(0, 0, 0)"
+          ></button>
+          Black
+        </div>
+        <div class="column">
+          <button
+            class="white color"
+            value="rgb(255, 255, 255)"
+            @click="handleColor($event)"
+            style="background-color: rgb(255, 255, 255)"
+          ></button>
+          White
+        </div>
+        <div class="column">
+          <button
+            id="lastColor"
+            class="last color"
+            :value="`${lastColor}`"
+            @click="handleColor($event)"
+            :style="`background-color: ${lastColor}`"
+          ></button>
+          Last
+        </div>
+      </div>
+      <div class="container-flex" id="gallery"></div>
     </div>
   </div>
-  <div class="container">
+
+  <div class="container-flex">
     <Canvas :size="24" :color="color"></Canvas>
   </div>
 </template>
