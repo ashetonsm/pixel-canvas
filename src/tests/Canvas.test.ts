d8927fc4a9fd131252edc9c7147ffafba33f76bf
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-17 20:49:04 +0900

    53.33% coverage.

--- src/tests/Canvas.test.ts
+++ src/tests/Canvas.test.ts
@@ -1,5 +1,16 @@
-import { it } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { beforeAll, describe, it } from 'vitest'
+import Canvas from '@/components/Canvas.vue'
 
-it('Canvas.vue loads', ({ expect }) => {
-  expect(2 + 2).toBe(4)
+let wrapper: any
+
+describe('Canvas', () => {
+  beforeAll(() => {
+    wrapper = mount(Canvas, { props: { size: 24, color: "black" } })
+  })
+
+  it('Canvas exists', ({ expect }) => {
+    expect(wrapper.exists()).toBe(true)
+  })
+
 })
