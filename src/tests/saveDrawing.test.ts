c410f553644985c5af638be779e45b5d11516ee1
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-17 18:11:21 +0900

    Work in progress on working with the document object.

--- src/tests/saveDrawing.test.ts
+++ src/tests/saveDrawing.test.ts
@@ -1,5 +1,32 @@
-import { it } from 'vitest'
+import { saveDrawing } from '@/tools/saveDrawing'
+import { expectTypeOf, it, test, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
 
-it('CanvasDisplay.vue loads', ({ expect }) => {
-  expect(4).toBe(4)
+it('saveDrawing.ts exists', ({ expect }) => {
+  Object.defineProperty(document.body, 'classList', {
+    value: {
+      remove: vi.fn()
+    },
+    writable: true
+  })
+
+  const wrapper = mount(saveDrawing)
+  
+  
+  expect(saveDrawing("TestArt"))
+})
+
+test('async callback', async () => {
+
+  // This is the mock function
+  const myMockFn = vi.fn(() => 'original')
+
+  await myMockFn.withImplementation(
+    () => 'temp',
+    async () => {
+      myMockFn()  // 'temp'
+    },
+  )
+
+  myMockFn()
 })
