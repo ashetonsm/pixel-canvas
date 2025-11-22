f4c80311ddccc3f970d9ed7036a2202805e39a10
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-22 16:17:13 +0900

    62.06% coverage.

--- src/tests/CanvasDisplay.test.ts
+++ src/tests/CanvasDisplay.test.ts
@@ -1,5 +1,71 @@
-import { it } from 'vitest'
+import CanvasDisplay from '@/components/CanvasDisplay.vue';
+import { mount, VueWrapper } from '@vue/test-utils';
+import { beforeAll, describe, it, vi } from 'vitest'
+import { ref } from 'vue';
 
-it('CanvasDisplay.vue loads', ({ expect }) => {
-  expect(4).toBe(4)
+let displayWrapper: VueWrapper | null
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+let ctx: CanvasRenderingContext2D | null
+
+describe('Canvas', () => {
+
+  beforeAll(() => {
+    displayWrapper = mount(CanvasDisplay)
+    canvasRef.value = displayWrapper.find('canvas').element
+    if (canvasRef.value) {
+      ctx = canvasRef.value.getContext("2d")
+      canvasRef.value.width = 100
+      canvasRef.value.height = 100
+    }
+  })
+
+  it('should find CanvasDisplay.vue', ({ expect }) => {
+    expect(displayWrapper).not.toBeNull()
+  })
+
+  it('should successfully call resetCanvas', async ({ expect }) => {
+    console.log('canvasRef.value: ', canvasRef.value)
+    const displayObject = displayWrapper!.vm
+
+    // Create a spy on resetCanvas
+    const spy = vi.spyOn(displayObject as any, 'resetCanvas')
+
+    // Simulate calling the method
+    displayObject.resetCanvas(Event)
+
+    // Assert that resetCanvas was called
+    expect(spy).toHaveBeenCalled()
+  })
+
+  it('should successfully call handleColor', async ({ expect }) => {
+    const displayObject = displayWrapper!.vm
+
+    // Create a spy on handleColor
+    const spy = vi.spyOn(displayObject as any, 'handleColor')
+
+    const e = new Event('click')
+    const et = new EventTarget()
+
+    // Simulate calling the method
+    displayObject.handleColor(et)
+
+    // Assert that handleColor was called
+    expect(spy).toHaveBeenCalled()
+    expect(displayObject.lastColor).toBe("rgb(0, 0, 0)")
+    expect(displayObject.color).toBe("rgb(0, 0, 0)")
+  })
+
+  it('should successfully call handleSubmit', async ({ expect }) => {
+    const displayObject = displayWrapper!.vm
+
+    // Create a spy on pixelPreview
+    const spy = vi.spyOn(displayObject as any, 'handleSubmit')
+
+    // Simulate calling the method
+    displayObject.handleSubmit()
+
+    // Assert that previewPixel was called
+    expect(spy).toHaveBeenCalled()
+  })
+
 })
