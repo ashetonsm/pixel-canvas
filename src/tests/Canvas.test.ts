0d7f83408d08f2b037871522fd8cf4abf9dd084a
Asheton Mayfield <98793112+ashetonsm@users.noreply.github.com>
2025-11-22 15:23:12 +0900

    96.66% coverage even though ts says the methods don't exist on the vi.spyOn types.

--- src/tests/Canvas.test.ts
+++ src/tests/Canvas.test.ts
@@ -1,9 +1,9 @@
-import { mount } from '@vue/test-utils'
-import { beforeAll, describe, expect, it } from 'vitest'
+import { mount, VueWrapper } from '@vue/test-utils'
+import { beforeAll, describe, expect, it, vi } from 'vitest'
 import Canvas from '@/components/Canvas.vue'
 import { ref } from 'vue';
 
-let wrapper: any
+let wrapper: VueWrapper | null
 const canvasRef = ref<HTMLCanvasElement | null>(null);
 let ctx: CanvasRenderingContext2D | null
 
@@ -11,13 +11,12 @@ let ctx: CanvasRenderingContext2D | null
 describe('Canvas', () => {
   beforeAll(() => {
     wrapper = mount(Canvas, { props: { size: 24, color: "black" } })
-    canvasRef.value = wrapper.find('canvas').wrapperElement
+    canvasRef.value = wrapper.find('canvas').element
     if (canvasRef.value) {
       ctx = canvasRef.value.getContext("2d")
       canvasRef.value.width = 100
       canvasRef.value.height = 100
     }
-
   })
 
   it('should return the canvas value', ({ expect }) => {
@@ -31,27 +30,43 @@ describe('Canvas', () => {
   })
 
   it('should show the prop color is black', ({ expect }) => {
-    expect(wrapper.props("color")).toBe("black")
-    console.log(wrapper.props("color"))
+    expect(wrapper?.props()).contains({ color: "black" })
+    console.log(wrapper?.props())
   })
 
-
-  describe('Canvas methods', async () => {
-
-    it('should return click.value true on click'), async () => {
-      // set click value to false
-      wrapper.vm.click.value = false
-      console.log("wrapper.vm.click.value: ", wrapper.vm.click.value)
-      // Trigger a mousedown event
-      const t = await wrapper.trigger('mousedown')
-        .then(() => {
-          console.log("complete")
-        })
-      
-      expect(wrapper.vm.click.value).toBe(true)
-      console.log("wrapper.vm.click.value: ", wrapper.vm.click.value)
-    }
+  it('should return click.value true via mouseDown', async ({ expect }) => {
+    wrapper?.vm.mouseDown()
+    expect(wrapper?.vm.click).true
   })
 
+  it('should return click.value false via mouseUp', async ({ expect }) => {
+    wrapper?.vm.mouseUp()
+    expect(wrapper!.vm.click).false
+  })
 
+  it('should successfully call previewPixel', async ({ expect }) => {
+    const canvasObject = wrapper!.vm
+
+    // Create a spy on pixelPreview
+    const spy = vi.spyOn(canvasObject as any, 'previewPixel')
+
+    // Simulate calling the method
+    canvasObject.previewPixel(Event)
+
+    // Assert that previewPixel was called
+    expect(spy).toHaveBeenCalled()
+  })
+
+  it('should successfully call draw', async ({ expect }) => {
+    const canvasObject = wrapper!.vm
+
+    // Create a spy on draw
+    const spy = vi.spyOn(canvasObject as any, 'draw')
+
+    // Simulate calling the method
+    canvasObject.draw(ctx, Event)
+
+    // Assert that draw was called
+    expect(spy).toHaveBeenCalled()
+  })
 })
